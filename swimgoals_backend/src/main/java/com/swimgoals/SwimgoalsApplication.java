c8abb51af1b99dbd05abf6d63ed47694ded062c8
FaresBza <fares.bouzaria@crea-inseec.com>
2025-02-19 11:45:19 +0100

    Spring Boot project initialized

---
+++ swimgoals_backend/src/main/java/com/swimgoals/SwimgoalsApplication.java
@@ -0,0 +1,13 @@
+package com.swimgoals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwimgoalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwimgoalsApplication.class, args);
+	}
+
+}
---
+++ swimgoals_backend/src/test/java/com/swimgoals/SwimgoalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swimgoals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwimgoalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
