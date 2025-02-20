f169c59b90150a7701eb468f7690fb524b5ea561
FaresBza <fares.bouzaria@crea-inseec.com>
2025-02-20 15:55:27 +0100

    Add User and Role in Model package

---
+++ swimgoals_backend/src/main/java/com/swimgoals/models/Role.java
@@ -0,0 +1,44 @@
+package com.swimgoals.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @Column(name = "id", nullable = false, unique = true)
+    private int id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    public Role() {
+        this(0, "");
+    }
+
+    public Role(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public static final Role Admin = new Role(1, "admin");
+    public static final Role Coach = new Role(2, "coach");
+    public static final Role Swimmer = new Role(3, "swimmer");
+}
---
+++ swimgoals_backend/src/main/java/com/swimgoals/models/User.java
@@ -0,0 +1,105 @@
+package com.swimgoals.models;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+    
+    @Id
+    @Column(length = 16, nullable = false, unique = true)
+    private UUID id;
+
+    @Column(name = "lastname", nullable = false)
+    private String lastname;
+
+    @Column(name = "firstname", nullable = false)
+    private String firstname;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id", foreignKey = @ForeignKey(name = "fk_user_role"))
+    private Role role;
+
+    @PrePersist
+    public void generateId() {
+        if (this.id == null) {
+            this.id = UUID.randomUUID();
+        }
+    }
+
+    public User() {
+        this(UUID.randomUUID(), "", "", "", "", null);
+    }
+
+    public User(UUID id, String lastname, String firstname, String email, String password, Role role) {
+        this.id = id;
+        this.lastname = lastname;
+        this.firstname = firstname;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+    
+}
