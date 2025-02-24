7d80988d9a30be7f8c5b9bd273205e48ca4e70ba
FaresBza <fares.bouzaria@crea-inseec.com>
2025-02-24 10:25:37 +0100

    Create an interface for UserService

---
+++ swimgoals_backend/src/main/java/com/swimgoals/interfaces/UserService.java
@@ -0,0 +1,26 @@
+package com.swimgoals.interfaces;
+
+import java.util.NoSuchElementException;
+
+import com.swimgoals.models.User;
+
+public interface UserService {
+
+    /**
+     * 
+     * @param user
+     * @return L'utilisateur enregistré si l'email est unique
+     * @throws IllegalArgumentException
+     */
+    public User registUser(User user);
+
+    /**
+     * 
+     * @param email
+     * @param password
+     * @return L'utilisateur si les informations d'identification sont valides
+     * @throws NoSuchElementException
+     * @throws IllegalArgumentException
+     */
+    public User loginUser(String email, String password);
+}
--- swimgoals_backend/src/main/java/com/swimgoals/service/UserServiceImpl.java
+++ swimgoals_backend/src/main/java/com/swimgoals/service/UserServiceImpl.java
@@ -7,17 +7,14 @@ import java.util.NoSuchElementException;
 
 import org.springframework.stereotype.Service;
 
+import com.swimgoals.interfaces.UserService;
+
 @Service
-public class UserService {
+public class UserServiceImpl implements UserService{
 
     private UserRepository userRepository;
 
-    /**
-     * 
-     * @param user
-     * @return L'utilisateur enregistré si l'email est unique
-     * @throws IllegalArgumentException
-     */
+    @Override
     public User registUser(User user) throws IllegalArgumentException{
         if(userRepository.existsByEmail(user.getEmail()) != null) {
             throw new IllegalArgumentException("Email is already exists");
@@ -25,14 +22,7 @@ public class UserService {
         return userRepository.save(user);
     }
 
-    /**
-     * 
-     * @param email
-     * @param password
-     * @return L'utilisateur si les informations d'identification sont valides
-     * @throws NoSuchElementException
-     * @throws IllegalArgumentException
-     */
+    @Override
     public User loginUser(String email, String password) throws NoSuchElementException, IllegalArgumentException {
         User user = userRepository.findByEmail(email);
         if (user == null){
