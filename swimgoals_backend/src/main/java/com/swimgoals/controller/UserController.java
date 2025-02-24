2ef5e8a014b02cf317c89b90906e8ec36f35f6b0
FaresBza <fares.bouzaria@crea-inseec.com>
2025-02-24 14:25:05 +0100

    Add documentation Swagger

--- swimgoals_backend/src/main/java/com/swimgoals/controller/UserController.java
+++ swimgoals_backend/src/main/java/com/swimgoals/controller/UserController.java
@@ -8,9 +8,14 @@ import org.springframework.web.bind.annotation.RestController;
 import com.swimgoals.models.User;
 import com.swimgoals.service.UserServiceImpl;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 
+@Tag(name = "User Authentification", description = "GET and POST methos for users authentification")
 @RestController
 public class UserController {
 
@@ -20,6 +25,10 @@ public class UserController {
         this.userServiceImpl = userServiceImpl;
     }
 
+    @Operation(summary = "Register a new user", description = "Creates a new user in the database and returns the created user object.", tags = {
+            "User Authentication" })
+    @ApiResponse(responseCode = "200", description = "User registered successfully", content = @Content(schema = @Schema(implementation = User.class), mediaType = "application/json"))
+    @ApiResponse(responseCode = "400", description = "Bad request", content = @Content(schema = @Schema()))
     @PostMapping("/register")
     public ResponseEntity<User> registerUser(@RequestBody User user) {
         try {
@@ -30,10 +39,14 @@ public class UserController {
         }
     }
 
+    @Operation(summary = "User login", description = "Authenticates the user and returns the user object if successful.", tags = {
+            "User Authentication" })
+    @ApiResponse(responseCode = "200", description = "User logged in successfully", content = @Content(schema = @Schema(implementation = User.class), mediaType = "application/json"))
+    @ApiResponse(responseCode = "400", description = "Invalid credentials", content = @Content(schema = @Schema()))
     @PostMapping("/login")
-    public ResponseEntity<User> loginUser(String email, String password) {
+    public ResponseEntity<User> loginUser(@RequestBody User user) {
         try {
-            User loggedUser = userServiceImpl.loginUser(email, password);
+            User loggedUser = userServiceImpl.loginUser(user.getEmail(), user.getPassword());
             return ResponseEntity.ok(loggedUser);
         } catch (IllegalArgumentException | IllegalStateException e) {
             return ResponseEntity.badRequest().body(null);
