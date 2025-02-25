0d4bf3c34732d5e4ed6b16bdff8033b0523e0692
FaresBza <fares.bouzaria@crea-inseec.com>
2025-02-25 17:14:18 +0100

    implements methods for group service

--- swimgoals_backend/src/main/java/com/swimgoals/service/GroupServiceImpl.java
+++ swimgoals_backend/src/main/java/com/swimgoals/service/GroupServiceImpl.java
@@ -1,42 +1,61 @@
 package com.swimgoals.service;
 
 import java.util.List;
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
 
 import com.swimgoals.interfaces.GroupService;
 import com.swimgoals.models.Group;
 import com.swimgoals.repository.GroupRepository;
 
+@Service
 public class GroupServiceImpl implements GroupService {
 
     private final GroupRepository groupRepository;
 
-    public GroupServiceImpl(GroupRepository groupRepository){
+    public GroupServiceImpl(GroupRepository groupRepository) {
         this.groupRepository = groupRepository;
     }
 
     @Override
     public List<Group> getAllGroups() {
-        throw new UnsupportedOperationException("Unimplemented method 'getAllGroups'");
+        return groupRepository.findAll();
     }
 
     @Override
-    public List<Group> getGroupsByCoachId(Integer coachId) {
-        throw new UnsupportedOperationException("Unimplemented method 'getGroupsByCoachId'");
+    public List<Group> getAllGroupsByCoachId(UUID coachId) {
+        return groupRepository.findByCoach(coachId);
     }
 
     @Override
-    public Group createGroupe(Group group) {
-        throw new UnsupportedOperationException("Unimplemented method 'createGroupe'");
+    public Group createGroup(Group group) {
+        if (group.getName() == null || group.getName().isEmpty()) {
+            throw new IllegalArgumentException("Le nom du groupe ne peut pas être vide.");
+        }
+        return groupRepository.save(group);
     }
 
     @Override
     public Group updateGroup(Integer groupId, Group group) {
-        throw new UnsupportedOperationException("Unimplemented method 'updateGroup'");
+        if (group.getName() == null || group.getName().isEmpty()) {
+            throw new IllegalArgumentException("Le nom du groupe ne peut pas être vide.");
+        }
+
+        return groupRepository.findById(groupId)
+                .map(existingGroup -> {
+                    existingGroup.setName(group.getName());
+                    existingGroup.setCoach(group.getCoach());
+                    return groupRepository.save(existingGroup);
+                })
+                .orElseThrow(() -> new IllegalArgumentException("Groupe non trouvé avec l'ID: " + groupId));
     }
 
     @Override
     public void deleteGroup(Integer groupId) {
-        throw new UnsupportedOperationException("Unimplemented method 'deleteGroup'");
+        if (!groupRepository.existsById(groupId)) {
+            throw new IllegalArgumentException("Le groupe d'ID " + groupId + " est introuvable.");
+        }
+        groupRepository.deleteById(groupId);
     }
-
 }
