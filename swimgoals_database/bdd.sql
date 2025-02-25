2a7f2f83b856ff4dd68705a3aac932bbc24e3fc2
FaresBza <fares.bouzaria@crea-inseec.com>
2025-02-25 14:11:20 +0100

    Change id users type CHAR(16) to BINARY(16)

--- swimgoals_database/bdd.sql
+++ swimgoals_database/bdd.sql
@@ -1,67 +1,60 @@
-CREATE TABLE Roles (
+CREATE TABLE roles (
     id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE Users (
-    id CHAR(16) PRIMARY KEY,
+CREATE TABLE users (
+    id BINARY(16) PRIMARY KEY,
     lastname VARCHAR(255) NOT NULL,
     firstname VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     role_id INT,
-    FOREIGN KEY (role_id) REFERENCES Roles(id),
-    INDEX idx_role_id (role_id)
+    FOREIGN KEY (role_id) REFERENCES roles(id)
 );
 
-CREATE TABLE Groups (
-    id CHAR(16) PRIMARY KEY,
-    coach_id CHAR(16),
+CREATE TABLE groups (
+    id INT PRIMARY KEY,
+    coach_id BINARY(16),
     name VARCHAR(255) NOT NULL,
-    FOREIGN KEY (coach_id) REFERENCES Users(id),
-    INDEX idx_coach_id (coach_id)
+    FOREIGN KEY (coach_id) REFERENCES Users(id)
 );
 
-CREATE TABLE Swimmers_group (
-    swimmer_id CHAR(16),
-    group_id CHAR(16),
+CREATE TABLE swimmers_group (
+    swimmer_id BINARY(16),
+    group_id INT,
     PRIMARY KEY (swimmer_id, group_id),
-    FOREIGN KEY (swimmer_id) REFERENCES Users(id),
-    FOREIGN KEY (group_id) REFERENCES Groups(id),
-    INDEX idx_swimmer_id (swimmer_id),
-    INDEX idx_group_id (group_id)
+    FOREIGN KEY (swimmer_id) REFERENCES users(id),
+    FOREIGN KEY (group_id) REFERENCES groups(id)
 );
 
-CREATE TABLE Swims (
+CREATE TABLE swims (
     id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE Objectives (
-    id CHAR(16) PRIMARY KEY,
-    swimmer_id  CHAR(16),
+CREATE TABLE objectives (
+    id INT PRIMARY KEY,
+    swimmer_id  BINARY(16),
     swim_id INT,
     distance VARCHAR(255) NOT NULL,
     time TIME NOT NULL,
-    FOREIGN KEY (swimmer_id) REFERENCES Users(id),
-    FOREIGN KEY (swim_id) REFERENCES Swims(id),
-    INDEX idx_swimmer_id (swimmer_id),
-    INDEX idx_swim_id (swim_id)
+    FOREIGN KEY (swimmer_id) REFERENCES users(id),
+    FOREIGN KEY (swim_id) REFERENCES swims(id)
 );
 
-CREATE TABLE Goals (
-    id CHAR(16) PRIMARY KEY,
-    objective_id CHAR(16),
+CREATE TABLE goals (
+    id INT PRIMARY KEY,
+    objective_id INT,
     time TIME NOT NULL,
     date DATE NOT NULL,
-    FOREIGN KEY (objective_id) REFERENCES Objectives(id),
-    INDEX idx_objective_id (objective_id)
+    FOREIGN KEY (objective_id) REFERENCES objectives(id)
 );
 
-INSERT INTO Roles VALUES 
+INSERT INTO roles VALUES 
 (1, "admin"), (2, "coach"), (3, "swimmer");
 
-INSERT INTO Swims VALUES 
+INSERT INTO swims VALUES 
 (1, "Papillon"), (2, "Dos"), (3, "Brasse"), 
 (4, "Crawl"), (5, "4 Nages"), (6, "Jambes"), 
 (7, "Nage Libre");
