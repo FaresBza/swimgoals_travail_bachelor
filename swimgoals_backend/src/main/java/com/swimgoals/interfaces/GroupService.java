b62a2fa32ba3f1e29c0a8f0a59598ed04d4d8835
FaresBza <fares.bouzaria@crea-inseec.com>
2025-02-25 15:51:08 +0100

    Create interface for group service

---
+++ swimgoals_backend/src/main/java/com/swimgoals/interfaces/GroupService.java
@@ -0,0 +1,18 @@
+package com.swimgoals.interfaces;
+
+import java.util.List;
+
+import com.swimgoals.models.Group;
+
+public interface GroupService {
+
+    List<Group> getAllGroups();
+
+    List<Group> getGroupsByCoachId(Integer coachId);
+
+    Group createGroupe(Group group);
+
+    Group updateGroup(Integer groupId, Group group);
+
+    void deleteGroup(Integer groupId);
+}
