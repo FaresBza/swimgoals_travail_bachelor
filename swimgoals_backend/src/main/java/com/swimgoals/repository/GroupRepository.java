c607caab3371e4dc20151cd54ad5af33c1617f28
FaresBza <fares.bouzaria@crea-inseec.com>
2025-02-26 13:54:29 +0100

    Hidden default endpoint with RepositoryRestResource in repositories

--- swimgoals_backend/src/main/java/com/swimgoals/repository/GroupRepository.java
+++ swimgoals_backend/src/main/java/com/swimgoals/repository/GroupRepository.java
@@ -4,9 +4,13 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
 
 import com.swimgoals.models.Group;
 
+@RepositoryRestResource(exported = false)
+@Repository
 public interface GroupRepository extends JpaRepository<Group, Integer>{
 
     List<Group> findByCoachId(UUID coach);
--- swimgoals_backend/src/main/java/com/swimgoals/repository/UserRepository.java
+++ swimgoals_backend/src/main/java/com/swimgoals/repository/UserRepository.java
@@ -3,11 +3,12 @@ package com.swimgoals.repository;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 import com.swimgoals.models.User;
 
-
+@RepositoryRestResource(exported = false)
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
     
