0414bd2328a8ed4c266670992f8efcba6931e58f
FaresBza <fares.bouzaria@crea-inseec.com>
2025-02-27 16:04:56 +0100

    Create a style component button

---
+++ swimgoals_frontend/src/app/components/Button.tsx
@@ -0,0 +1,12 @@
+import './../styles/Button.scss'
+
+const Button = () => {
+    return (
+        <button className="button">
+            
+        </button>
+    )
+    
+}
+
+export default Button;
