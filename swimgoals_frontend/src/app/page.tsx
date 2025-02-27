ac9347bde91dc3574dc491585e367073ed7b42d7
FaresBza <fares.bouzaria@crea-inseec.com>
2025-02-27 16:06:39 +0100

    Add style for home page

--- swimgoals_frontend/src/app/page.tsx
+++ swimgoals_frontend/src/app/page.tsx
@@ -1,10 +1,16 @@
+import Button from "./components/Button";
 import Header from "./components/Header";
+
+import './styles/Home.scss'
 import './styles/BackgroundImage.scss'
 
 export default function Home() {
   return (
     <div className="container">
       <Header />
+      <div className="button-container">
+        <Button />
+      </div>
     </div>
   )
 }
