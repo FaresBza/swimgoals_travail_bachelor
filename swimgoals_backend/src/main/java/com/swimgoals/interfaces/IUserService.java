3874a4f06065957ade6cc845de55e704b3443f21
FaresBza <fares.bouzaria@crea-inseec.com>
2025-03-03 14:14:00 +0100

    Chnage signature of service interface

--- swimgoals_backend/src/main/java/com/swimgoals/interfaces/IUserService.java
+++ swimgoals_backend/src/main/java/com/swimgoals/interfaces/IUserService.java
@@ -1,10 +1,11 @@
 package com.swimgoals.interfaces;
 
+import com.swimgoals.dto.UserDTO;
 import com.swimgoals.models.User;
 
 public interface IUserService {
 
-    public User registUser(User user);
+    public User registUser(UserDTO userDTO);
 
     public User loginUser(String email, String password);
 }
