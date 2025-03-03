2ad22d8d033e19206bdb39aac7fcfd0dd2585d50
FaresBza <fares.bouzaria@crea-inseec.com>
2025-03-03 14:08:46 +0100

    Create a user dto using in the backend for synch database

---
+++ swimgoals_backend/src/main/java/com/swimgoals/dto/UserDTO.java
@@ -0,0 +1,9 @@
+package com.swimgoals.dto;
+
+public class UserDTO {
+    public String firstname;
+    public String lastname;
+    public String email;
+    public String password;
+    public Integer roleId;
+}
