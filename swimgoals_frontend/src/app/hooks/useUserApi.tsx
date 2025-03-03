19dcc5fd3d102ae51924752ad1a931064a1d805e
FaresBza <fares.bouzaria@crea-inseec.com>
2025-03-03 12:18:55 +0100

    Add handleRegister method in custom hook useUserApi

--- swimgoals_frontend/src/app/hooks/useUserApi.tsx
+++ swimgoals_frontend/src/app/hooks/useUserApi.tsx
@@ -1,8 +1,34 @@
-
+interface UserData {
+    firstname: string;
+    lastname: string;
+    email: string;
+    password: string;
+}
 
 const useUserApi = () => {
+    const handleRegister = async ({ firstname, lastname, email, password }: UserData): Promise<void> => {
+        const newUser = { firstname, lastname, email, password };
 
-    
+        try {
+            const response = await fetch("http://localhost:8080/api/register", {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(newUser)
+            });
+
+            if (response.ok) {
+                console.log("Création de compte REUSSI !");
+            } else {
+                console.error("Erreur lors de l'inscription");
+            }
+        } catch (error) {
+            console.error("Erreur réseau :", error);
+        }
+    };
+
+    return { handleRegister };
 };
 
 export default useUserApi;
