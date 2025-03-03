6ce86c0baaa7e4a102e9a8ab119ae9b9604dfcc3
FaresBza <fares.bouzaria@crea-inseec.com>
2025-03-03 14:14:21 +0100

    new logical for role id

--- swimgoals_backend/src/main/java/com/swimgoals/service/UserService.java
+++ swimgoals_backend/src/main/java/com/swimgoals/service/UserService.java
@@ -4,27 +4,44 @@ import java.util.NoSuchElementException;
 
 import org.springframework.stereotype.Service;
 
+import com.swimgoals.dto.UserDTO;
 import com.swimgoals.interfaces.IUserService;
+import com.swimgoals.models.Role;
 import com.swimgoals.models.User;
+import com.swimgoals.repository.RoleRepository;
 import com.swimgoals.repository.UserRepository;
 
 @Service
 public class UserService implements IUserService{
 
     private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
     
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, RoleRepository roleRepository) {
         this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
     }
 
     @Override
-    public User registUser(User user) throws IllegalArgumentException{
-        var existEmailUser = userRepository.existsByEmail(user.getEmail());
-        if(existEmailUser == null) {
-            throw new IllegalArgumentException("Email is already exists");
+    public User registUser(UserDTO userDTO) throws IllegalArgumentException {
+        var existEmailUser = userRepository.existsByEmail(userDTO.email);
+        if (existEmailUser) {
+            throw new IllegalArgumentException("Email already exists");
         }
+
+        Role role = roleRepository.findById(userDTO.roleId)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid role ID"));
+
+        User user = new User();
+        user.setFirstname(userDTO.firstname);
+        user.setLastname(userDTO.lastname);
+        user.setEmail(userDTO.email);
+        user.setPassword(userDTO.password);
+        user.setRole(role);
+
         return userRepository.save(user);
-    }
+}
+
 
     @Override
     public User loginUser(String email, String password) throws NoSuchElementException, IllegalArgumentException {
