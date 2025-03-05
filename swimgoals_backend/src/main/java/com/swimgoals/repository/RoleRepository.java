e2ae1368f52f8db72463975d824425deaae101ae
FaresBza <fares.bouzaria@crea-inseec.com>
2025-03-05 10:22:49 +0100

    Added `@Repository` and `@RepositoryRestResource` annotations in `RoleRepository` to enhance role management.

--- swimgoals_backend/src/main/java/com/swimgoals/repository/RoleRepository.java
+++ swimgoals_backend/src/main/java/com/swimgoals/repository/RoleRepository.java
@@ -1,9 +1,13 @@
 package com.swimgoals.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
 
 import com.swimgoals.models.Role;
 
+@Repository
+@RepositoryRestResource(exported = false)
 public interface RoleRepository extends JpaRepository<Role, Integer> {
 
 }
