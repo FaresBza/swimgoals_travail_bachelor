3639ccf1ac4852733805e3f1937f280647d029f0
FaresBza <fares.bouzaria@crea-inseec.com>
2025-03-06 11:21:54 +0100

    Initialized 2 pages Home and HomeAdmin

---
+++ swimgoals_frontend/src/app/pages/home-admin/index.tsx
@@ -0,0 +1,9 @@
+
+
+const HomeAdmin = () => {
+    return (
+        <h1>Bienvenue sur la page administratif</h1>
+    );
+}
+
+export default HomeAdmin;
---
+++ swimgoals_frontend/src/app/pages/home/index.tsx
@@ -0,0 +1,8 @@
+
+const Home = () => {
+    return (
+        <h1>Bienvenue sur la page des coachs et swimmers</h1>
+    );
+}
+
+export default Home;
