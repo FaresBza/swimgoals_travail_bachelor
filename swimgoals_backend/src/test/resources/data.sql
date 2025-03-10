7a80ef389afe1bed84763db8dcc88ca20e26668a
FaresBza <fares.bouzaria@crea-inseec.com>
2025-03-10 09:26:09 +0100

    Insert sql request for users

--- swimgoals_backend/src/test/resources/data.sql
+++ swimgoals_backend/src/test/resources/data.sql
@@ -1,6 +1,9 @@
-CREATE TABLE IF NOT EXISTS ROLES ( id INT PRIMARY KEY, name VARCHAR(255) NOT NULL );
-
-
 INSERT INTO ROLES(id, name) VALUES (1, 'admin');
 INSERT INTO ROLES(id, name) VALUES (2, 'coach');
 INSERT INTO ROLES(id, name) VALUES (3, 'swimmer');
+
+INSERT INTO USERS (lastname, firstname, email, password, role_id) VALUES ('Doe', 'John', 'john.doe@example.com', 'password123', 1);
+INSERT INTO USERS (lastname, firstname, email, password, role_id) VALUES ('Smith', 'Alice', 'alice.smith@example.com', 'securepass456', 2);
+INSERT INTO USERS (lastname, firstname, email, password, role_id) VALUES ('Brown', 'Michael', 'michael.brown@example.com', 'mypassword789', 3);
+INSERT INTO USERS (lastname, firstname, email, password, role_id) VALUES ('Johnson', 'Emily', 'emily.johnson@example.com', 'strongpass321', 3);
+INSERT INTO USERS (lastname, firstname, email, password, role_id) VALUES ('Williams', 'David', 'david.williams@example.com', 'supersecret654', 3);
