db4891f1a1331183e57d120c1fb1184fa78735d1
FaresBza <fares.bouzaria@crea-inseec.com>
2025-03-27 14:36:39 +0100

    Add form structure with input name only

--- swimgoals_frontend/src/app/components/FormGroup.tsx
+++ swimgoals_frontend/src/app/components/FormGroup.tsx
@@ -1,7 +1,44 @@
+import React from "react";
 
+interface FormGroupProp {
+    name: string;
+}
 
-const FormGroup = () => {
-    
+const FormGroup: React.FC<FormGroupProp> = () => {
+
+    return(
+        <div
+            className="add-group-container"
+            data-aos="fade-up"
+            data-aos-duration="800"
+        >
+            <div className="title-container" aria-labelledby="main-title">
+                <h1 className="title" id="main-title">Ajouter un groupe</h1>
+            </div>
+            <main className="main">
+                <form
+                            className="form"
+                        >
+                            <div className="form-group">
+                                <label className="name-label" htmlFor="name">Name</label>
+                                <input
+                                    type="text"
+                                    className="input"
+                                    placeholder="Name"
+                                    id="name"
+                                    required
+                                />
+                            </div>
+                        </form>
+                        <button
+                            type="submit" 
+                            className="submit-button"
+                        >
+                            Se connecter
+                        </button>
+            </main>
+        </div>
+    )
 }
 
 export default FormGroup;
