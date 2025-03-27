fbb64ef07757033995be47862d9f8d99fd1f1198
FaresBza <fares.bouzaria@crea-inseec.com>
2025-03-27 10:46:32 +0100

    Added retrieval and display of groups in the home page

--- swimgoals_frontend/src/app/home/page.tsx
+++ swimgoals_frontend/src/app/home/page.tsx
@@ -1,9 +1,42 @@
-"use client"
+"use client";
+
+import { useEffect, useState } from "react";
+import useGroupApi from "../hooks/useGroupApi";
 
 const Home = () => {
+    const [groups, setGroups] = useState<{ name: string }[]>([]);
+    const { fetchAllGroups } = useGroupApi();
+
+    const loadGroups = async () => {
+        try {
+            const data = await fetchAllGroups();
+            if (data && Array.isArray(data)) {
+                setGroups(data);
+            } else {
+                setGroups([]);
+            }
+        } catch (error) {
+            console.error("Erreur lors de la récupération des groupes :", error);
+            setGroups([]);
+        };
+    };
+
+    useEffect(() => {
+        loadGroups();
+    }, []);
+
     return (
-        <h1>Bienvenue sur la page des coachs et swimmers</h1>
+        <div>
+            <h1>Bienvenue sur la page des coachs et swimmers</h1>
+            <ul>
+                {groups.length > 0 ? (
+                    groups.map((group, index) => <li key={index}>{group.name}</li>)
+                ) : (
+                    <p>Aucun groupe trouvé.</p>
+                )}
+            </ul>
+        </div>
     );
-}
+};
 
 export default Home;
