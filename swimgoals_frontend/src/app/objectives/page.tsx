aeebaca913e259722a3fbc8f86ad3b71d5bd793c
FaresBza <fares.bouzaria@crea-inseec.com>
2025-04-24 17:13:46 +0200

    Create objective page

---
+++ swimgoals_frontend/src/app/objectives/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+const Objetcives = () => {
+    return (
+        <div>
+            <h1>Bienvenue sur la page de tes objetcifs</h1>
+        </div>
+    )
+}
+
+export default Objetcives;
