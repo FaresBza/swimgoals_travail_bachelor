8ae217ba2fb4d8e5ba7ac4c6462d0721f1d290b0
FaresBza <fares.bouzaria@crea-inseec.com>
2025-05-10 21:00:25 +0200

    Added logic to retrieve objectives by swimmer ID in the useObjectiveApi hook

--- swimgoals_frontend/src/app/hooks/useObjectiveApi.tsx
+++ swimgoals_frontend/src/app/hooks/useObjectiveApi.tsx
@@ -1,6 +1,36 @@
+import { useState } from "react";
+import ObjectiveData from "../data/ObjectiveData";
 
 
 const useObjectiveApi = () => {
+
+    const [objectives, setObjectives] = useState<ObjectiveData[]>([]);
+
+    const fetchObjectivesBySwimmerId = async ({ swimmerId }: { swimmerId: number }) => {
+        try {
+            const response = await fetch(`http://localhost:8080/api/objectives/swimmer/${swimmerId}`, {
+                method: "GET",
+                headers: {
+                    Accept: "application/json",
+                },
+            });
+
+            if(!response.ok) {
+                throw new Error(`Erreur HTTP: ${response.status}`);
+            }
+
+            setObjectives(await response.json());
+            return objectives;
+
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    return {
+        objectives,
+        fetchObjectivesBySwimmerId,
+    }
 
 }
 
