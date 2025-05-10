a55363627a962a417556f4b44e0d7d530b8796c0
FaresBza <fares.bouzaria@crea-inseec.com>
2025-05-10 20:25:57 +0200

    Add annotations @Entity and @Table to Objective model class

--- swimgoals_backend/src/main/java/com/swimgoals/models/Objective.java
+++ swimgoals_backend/src/main/java/com/swimgoals/models/Objective.java
@@ -1,11 +1,15 @@
 package com.swimgoals.models;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.ForeignKey;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "objectives")
 public class Objective {
     
     @Id
