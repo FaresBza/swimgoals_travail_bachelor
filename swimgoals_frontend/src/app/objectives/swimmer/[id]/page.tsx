a5d8cbd2cfc071ec13f84b704113bd3ddc317599
FaresBza <fares.bouzaria@crea-inseec.com>
2025-05-10 21:00:37 +0200

    Add logic to retrieve objectives by swimmer ID in the Objectives component

--- swimgoals_frontend/src/app/objectives/swimmer/[id]/page.tsx
+++ swimgoals_frontend/src/app/objectives/swimmer/[id]/page.tsx
@@ -1,13 +1,31 @@
 "use client";
 
+import useObjectiveApi from "@/app/hooks/useObjectiveApi";
 import { useParams } from "next/navigation";
+import { useEffect } from "react";
 
 const Objectives = () => {
     const { id } = useParams();
 
+    const { objectives, fetchObjectivesBySwimmerId } = useObjectiveApi();
+
+    useEffect(() => {
+        if (id && typeof id === "string") {
+            fetchObjectivesBySwimmerId({ swimmerId: parseInt(id, 10) });
+        }
+    }, [id])
+
     return (
         <div>
-            <h1>Bienvenue sur la page des objectifs du nageur {id}</h1>
+            {objectives.map((objective) => (
+                <div
+                    key={objective.id}
+                >
+                    <h2>{objective.swimId}</h2>
+                    <p>{objective.distance}</p>
+                    <p>{objective.time}</p>
+                </div>
+            ))}
         </div>
     );
 };
