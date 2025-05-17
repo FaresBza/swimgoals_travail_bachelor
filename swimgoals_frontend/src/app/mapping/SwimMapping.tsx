4606320da0be03d7feff1727f0398ec14f0ae49b
FaresBza <fares.bouzaria@crea-inseec.com>
2025-05-17 18:12:30 +0200

    Add SwimMapping map for different swimming styles

---
+++ swimgoals_frontend/src/app/mapping/SwimMapping.tsx
@@ -0,0 +1,11 @@
+const SwimMapping: Record<string, number> = {
+    papillon: 1,   
+    dos: 2,   
+    brasse: 3,   
+    crawl: 4,   
+    QuatreNages: 5,   
+    Jambes: 6,   
+    NageLibre: 7,   
+}
+
+export default SwimMapping;
