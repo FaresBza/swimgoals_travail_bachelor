c6c2e7a32937450740e2ba84b65db26ea7d26c35
FaresBza <fares.bouzaria@crea-inseec.com>
2025-05-18 15:23:51 +0200

    Add GoalController, GoalRepository and GoalService classes

---
+++ swimgoals_backend/src/main/java/com/swimgoals/repository/GoalRepository.java
@@ -0,0 +1,13 @@
+package com.swimgoals.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
+
+import com.swimgoals.models.Goal;
+
+@RepositoryRestResource(exported = false)
+@Repository
+public interface  GoalRepository extends JpaRepository<Goal, Integer> {
+    
+}
