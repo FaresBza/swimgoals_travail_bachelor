5d9ed2f9dc97b53b5baa2274c1a0ac72e6e608ed
FaresBza <fares.bouzaria@crea-inseec.com>
2025-06-20 16:23:44 +0200

    Added timer functionality with start and stop control methods in ObjectiveResult component

--- swimgoals_frontend/src/app/objectives/result/[objectiveId]/page.tsx
+++ swimgoals_frontend/src/app/objectives/result/[objectiveId]/page.tsx
@@ -1,13 +1,47 @@
 "use client"
 
+import useGoalsApi from "@/app/hooks/useGoalsAPI";
 import { useParams } from "next/navigation";
+import { useState, useRef } from "react";
+import TimerDisplay from "@/app/components/TimeDisplay";
+import TimerControls from "@/app/components/TimerControls";
 
 const ObjectiveResult = () => {
     const { objectiveId } = useParams();
+    const { createGoal } = useGoalsApi();
+
+    const [time, setTime] = useState(0);
+    const [running, setRunning] = useState(false);
+    const timer = useRef<NodeJS.Timeout | null>(null);
+
+    const start = () => {
+        if (timer.current) return;
+        setRunning(true);
+        timer.current = setInterval(() => setTime((t) => t + 10), 10);
+    };
+
+    const stop = () => {
+        if (!timer.current) return;
+        clearInterval(timer.current);
+        timer.current = null;
+        setRunning(false);
+
+        // format de la date : "YYYY-MM-DD"
+        const date = new Date().toISOString().split("T")[0];
+        const timeInSeconds = Math.floor(time / 1000).toString();
+
+        createGoal({
+        objectiveId: parseInt(objectiveId as string),
+        time: timeInSeconds,
+        date,
+        });
+    };
 
     return (
-        <div>
-            <h1>Résultat de objectif {objectiveId}</h1>
+        <div style={{ textAlign: "center", marginTop: 50 }}>
+        <h1>Résultat de l’objectif #{objectiveId}</h1>
+        <TimerDisplay time={time} />
+        <TimerControls isRunning={running} onStart={start} onStop={stop} />
         </div>
     );
 };
