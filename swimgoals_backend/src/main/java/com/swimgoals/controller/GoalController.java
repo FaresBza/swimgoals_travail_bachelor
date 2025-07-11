105d11dec65be91275bf0522d367f288e1d23f0a
FaresBza <fares.bouzaria@crea-inseec.com>
2025-07-11 17:26:16 +0200

    Added getGoalsByObjective method in GoalController to retrieve the goals of a specific objective

--- swimgoals_backend/src/main/java/com/swimgoals/controller/GoalController.java
+++ swimgoals_backend/src/main/java/com/swimgoals/controller/GoalController.java
@@ -1,7 +1,11 @@
 package com.swimgoals.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +28,16 @@ public class GoalController {
 
     public GoalController(GoalService goalService) {
         this.goalService = goalService;
+    }
+
+    @Operation(summary = "Retrieve all goals of one objective", description = "Retrieves a list of all goals of one objective available in the database", tags = {
+            "Goal" })
+    @ApiResponse(responseCode = "200", description = "List of goals of one objective retrieved successfully", content = @Content(schema = @Schema(implementation = Goal.class), mediaType = "application/json"))
+    @ApiResponse(responseCode = "400", description = "Invalid credentials", content = @Content(schema = @Schema()))
+    @GetMapping("/goals/{objectiveId}")
+    public ResponseEntity<List<Goal>> getGoalsByObjective(@PathVariable Integer objectiveId) {
+        List<Goal> goals = goalService.getGoalsByObjectiveId(objectiveId);
+        return ResponseEntity.ok(goals);
     }
 
     @Operation(summary = "Create a new goal of an objetive", description = "Creates a new goal of an objetive in the database and returns the created objective object", tags = {
