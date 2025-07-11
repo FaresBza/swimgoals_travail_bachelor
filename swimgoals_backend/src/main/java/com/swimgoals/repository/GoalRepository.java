ffb334f56abb04daa423b4668f2eb10735333104
FaresBza <fares.bouzaria@crea-inseec.com>
2025-07-11 17:25:09 +0200

    Added findByObjectiveId method in GoalRepository to retrieve the goals of an objective.

--- swimgoals_backend/src/main/java/com/swimgoals/repository/GoalRepository.java
+++ swimgoals_backend/src/main/java/com/swimgoals/repository/GoalRepository.java
@@ -1,5 +1,7 @@
 package com.swimgoals.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
@@ -10,4 +12,5 @@ import com.swimgoals.models.Goal;
 @Repository
 public interface GoalRepository extends JpaRepository<Goal, Integer> {
     
+    List<Goal> findByObjectiveId(Integer objectifId);
 }
