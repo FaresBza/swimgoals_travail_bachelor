95849438ed9f5e090e97fa9ca3b12a41ecbcb66d
FaresBza <fares.bouzaria@crea-inseec.com>
2025-07-13 20:01:48 +0200

    Add configuration options for the goal graph

---
+++ swimgoals_frontend/src/app/charts/objective/[id]/ChartOptions.ts
@@ -0,0 +1,19 @@
+export const chartOptions = {
+    responsive: true,
+    maintainAspectRatio: false,
+    scales: {
+        x: {
+            title: {
+                display: true,
+                text: "Date",
+            },
+        },
+        y: {
+            title: {
+                display: true,
+                text: "Temps (en secondes)",
+            },
+            
+        },
+    },
+};
