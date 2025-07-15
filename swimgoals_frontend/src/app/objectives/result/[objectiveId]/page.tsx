72bc8b5f2544f28cff3013c7d6e12b0abe7d639e
FaresBza <fares.bouzaria@crea-inseec.com>
2025-07-15 22:27:20 +0200

    Added fetching of objective details in ObjectiveResult component with error handling

--- swimgoals_frontend/src/app/objectives/result/[objectiveId]/page.tsx
+++ swimgoals_frontend/src/app/objectives/result/[objectiveId]/page.tsx
@@ -9,10 +9,15 @@ import useTimer from "@/app/utils/useTimer";
 import "@/app/styles/Timer.scss";
 import "./../../../styles/BackgroundImage.scss";
 import BackButton from "@/app/components/BackButton";
+import useObjectiveApi from "@/app/hooks/useObjectiveApi";
+import { useEffect, useState } from "react";
 
 const ObjectiveResult = () => {
     const { objectiveId } = useParams();
     const { createGoal } = useGoalsApi();
+    const { getObjectiveDetails, error } = useObjectiveApi();
+
+    const [objectiveDetail, setObjectiveDetail] = useState<string>("");
 
     const saveGoal = (timeInSeconds: number) => {
         const date = new Date().toISOString().split("T")[0];
@@ -24,13 +29,25 @@ const ObjectiveResult = () => {
         });
     };
 
+    const fetchObjectiveData = async () => {
+        if (objectiveId) {
+            const objectiveDetails = await getObjectiveDetails({ id: parseInt(objectiveId as string) });
+            setObjectiveDetail(objectiveDetails);
+        }
+    }
+
+    useEffect(() => {
+        fetchObjectiveData();
+    }, [objectiveId])
+
     const { time, running, start, stop, reset } = useTimer(saveGoal);
 
     return (
         <div className="container blur">
             <BackButton />
             <div className="timer-container">
-                <h1>Résultat de l’objectif #{objectiveId}</h1>
+                {error && <p style={{ color: "red" }}>{error}</p>}
+                <h1>Résultat {objectiveDetail}</h1>
                 <TimerDisplay time={time} />
                 <TimerControls
                     isRunning={running}
