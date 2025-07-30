7cdc5f7033002e1f0335f3418ed41fa13937e06d
FaresBza <fares.bouzaria@crea-inseec.com>
2025-07-30 11:48:57 +0200

    Fix capitalization of swim names in SwimMapping

--- swimgoals_frontend/src/app/mapping/SwimMapping.tsx
+++ swimgoals_frontend/src/app/mapping/SwimMapping.tsx
@@ -1,8 +1,8 @@
 const SwimMapping = [
-    { name: "papillon", id: 1 },
-    { name: "dos", id: 2 },
-    { name: "brasse", id: 3 },
-    { name: "crawl", id: 4 },
+    { name: "Papillon", id: 1 },
+    { name: "Dos", id: 2 },
+    { name: "Brasse", id: 3 },
+    { name: "Crawl", id: 4 },
     { name: "4 Nages", id: 5 },
     { name: "Jambes", id: 6 },
     { name: "Nage Libre", id: 7 },
