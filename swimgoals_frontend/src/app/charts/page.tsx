882fbb8a66d889eb4cfdeb3dd62389b6ff37b62c
FaresBza <fares.bouzaria@crea-inseec.com>
2025-08-05 18:09:40 +0200

    Refactor the ChartSwimmer component to improve structure and readability

--- swimgoals_frontend/src/app/charts/page.tsx
+++ swimgoals_frontend/src/app/charts/page.tsx
@@ -1,29 +1,40 @@
-"use client"
+"use client";
 
-import { useEffect } from "react";
+import { useState } from "react";
 import useLocalStorage from "../hooks/useLocalStorage";
 import useObjectiveApi from "../hooks/useObjectiveApi";
+import useGoalsApi from "../hooks/useGoalsAPI";
+import { useChartInitialization, useExtractObjectiveIds, useFetchGoalsByObjectives } from "../utils/useChartInitialization";
 
 const ChartSwimmer = () => {
-
     const { swimmerId, roleId, recoverUserId, recoverRoleId } = useLocalStorage();
     const { objectives, fetchObjectivesBySwimmerId } = useObjectiveApi();
+    const { goals, fetchGoalsByObjectifId } = useGoalsApi();
 
-    useEffect(() => {
-        recoverRoleId()
-        if(roleId) { recoverUserId() }
-        if(swimmerId) { fetchObjectivesBySwimmerId({ swimmerId }) }
-        
-    })
+    const [objectiveIds, setObjectiveIds] = useState<number[]>([]);
+
+    // Appels des hooks personnalisés
+    useChartInitialization(swimmerId, roleId, recoverUserId, recoverRoleId, fetchObjectivesBySwimmerId);
+    useExtractObjectiveIds(objectives, setObjectiveIds);
+    useFetchGoalsByObjectives(objectiveIds, fetchGoalsByObjectifId);
 
     return (
         <>
-            <h1>{swimmerId}</h1>
+            <h1>Swimmer ID: {swimmerId}</h1>
+            <p>Objective IDs: {objectiveIds.join(", ")}</p>
+
             {objectives.map(o => (
-                <h3 key={o.id}>{o.distance}m - {o.time}min</h3>
+                <div key={o.id}>
+                    <h3>{o.distance}m - {o.time}min</h3>
+                    {goals.map(g => (
+                        <div key={g.id}>
+                            <p>{g.date} - {g.time}</p>
+                        </div>
+                    ))}
+                </div>
             ))}
         </>
-    )
-}
+    );
+};
 
 export default ChartSwimmer;
