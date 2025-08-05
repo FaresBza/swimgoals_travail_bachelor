217f10a9d15e9eb3892b2b5fa2bf7327e11a9ce2
FaresBza <fares.bouzaria@crea-inseec.com>
2025-08-05 15:47:54 +0200

    Retrieve swimmer ID via local storage hook

--- swimgoals_frontend/src/app/charts/page.tsx
+++ swimgoals_frontend/src/app/charts/page.tsx
@@ -1,7 +1,22 @@
 "use client"
 
+import { useEffect } from "react";
+import useLocalStorage from "../hooks/useLocalStorage";
+
 const ChartSwimmer = () => {
 
+    const { swimmerId, roleId, recoverUserId, recoverRoleId } = useLocalStorage();
+
+    useEffect(() => {
+        recoverRoleId()
+        if(roleId) { recoverUserId() }
+    })
+
+    return (
+        <>
+            <h1>{swimmerId}</h1>
+        </>
+    )
 }
 
 export default ChartSwimmer;
