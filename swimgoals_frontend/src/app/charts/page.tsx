e5bf77cec72f3e326e9eb6ce7e6f58ae454df929
FaresBza <fares.bouzaria@crea-inseec.com>
2025-08-05 16:03:13 +0200

    Retrieve objectives via swimmer ID

--- swimgoals_frontend/src/app/charts/page.tsx
+++ swimgoals_frontend/src/app/charts/page.tsx
@@ -2,19 +2,26 @@
 
 import { useEffect } from "react";
 import useLocalStorage from "../hooks/useLocalStorage";
+import useObjectiveApi from "../hooks/useObjectiveApi";
 
 const ChartSwimmer = () => {
 
     const { swimmerId, roleId, recoverUserId, recoverRoleId } = useLocalStorage();
+    const { objectives, fetchObjectivesBySwimmerId } = useObjectiveApi();
 
     useEffect(() => {
         recoverRoleId()
         if(roleId) { recoverUserId() }
+        if(swimmerId) { fetchObjectivesBySwimmerId({ swimmerId }) }
+        
     })
 
     return (
         <>
             <h1>{swimmerId}</h1>
+            {objectives.map(o => (
+                <h3 key={o.id}>{o.distance}m - {o.time}min</h3>
+            ))}
         </>
     )
 }
